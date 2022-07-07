708219b5d88c83f30b397ae5967e1f0f8df5b713
Emmanuel Nwakanma <emmynetro@gmail.com>
2022-07-07 07:37:24 -0400

    fix: database url

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import os
+import dj_database_url
+
 load_dotenv(find_dotenv())
+
+environment = os.getenv("ENVIRONMENT", "Development")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,12 +84,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if environment == "Development":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+elif environment  == "Staging":
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config()
+
 
 
 # Password validation
