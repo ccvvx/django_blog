7321711a5f2a40cb7509460df8663015dcd0dd6c
Emmanuel Nwakanma <emmynetro@gmail.com>
2022-07-07 07:23:35 -0400

    fix: url

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogger/', include('blogger.urls', namespace='blogger')),
+    path('', include('blogger.urls', namespace='blogger')),
 ]
