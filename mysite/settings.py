90fd2c48525ce1c99bdab6e3f33722fb012172f3
Emmanuel Nwakanma <emmynetro@gmail.com>
2022-07-07 07:19:42 -0400

    fix: route

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["bloggist-travel.herokuapp.com"]
+ALLOWED_HOSTS = ['bloggist-travel.herokuapp.com']
 
 
 # Application definition
